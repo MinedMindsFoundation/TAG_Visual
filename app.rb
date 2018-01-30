c03330982e922837e4f63cecce23fa34a4f19c20
billyjack1988 <billyjacktattoos@gmail.com>
2018-01-30 14:25:18 -0500

    working on the db func and app.rb, pushing broken code for garrek

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require 'csv'
-
-
+require 'pg'
+require_relative 'databasefunc.rb'
+load './local_env.rb' if File.exist?('./local_env.rb')
 
 get '/' do 
     erb :choice
@@ -12,6 +13,8 @@ post '/choice' do
     if choice == "Tag_1"
         erb :tag_1
     elsif choice == "Tag_2"
+        check_connection()
+        info = getinfo()
         erb :tag_2
     end
 end
---
+++ database_func.rb
@@ -0,0 +1,37 @@
+require 'pg'
+
+load './local_env.rb' if File.exist?('./local_env.rb')
+
+def check_connection()
+    db_params = {
+        host: ENV['host'],
+        port: ENV['port'],
+        dbname: ENV['dbname'],
+        user: ENV['user'],
+        password: ENV['password']
+    }
+
+    db= PG::Connection.new(db_params)
+    p "connected"
+    db.close
+end
+
+def getinfo()
+    db_params = {
+        host: ENV['host'],
+        port: ENV['port'],
+        dbname: ENV['dbname'],
+        user: ENV['user'],
+        password: ENV['password']
+    }
+    db= PG::Connection.new(db_params)
+    data = [] 
+	db.exec( "SELECT * FROM tag_rx_table" ) do |result|
+	      result.each do |row|
+	          data << row.values
+	        end
+	         p "#{data} data is here"
+        end
+    db.close    
+	data    
+end
